d197a1046cf3fb73abeff66e6240b7ec11e01be5
Adam Smith <ismith.adam@gmail.com>
2016-07-09 08:10:23 -0400

    downcase tokens

--- lib/has_secure_token.rb
+++ lib/has_secure_token.rb
@@ -4,26 +4,7 @@ module ActiveRecord
     extend ActiveSupport::Concern
 
     module ClassMethods
-      # Example using has_secure_token
-      #
-      #   # Schema: User(token:string, auth_token:string)
-      #   class User < ActiveRecord::Base
-      #     has_secure_token
-      #     has_secure_token :auth_token
-      #   end
-      #
-      #   user = User.new
-      #   user.save
-      #   user.token # => "pX27zsMN2ViQKta1bGfLmVJE"
-      #   user.auth_token # => "77TMHrHJFvFDwodq8w7Ev2m7"
-      #   user.regenerate_token # => true
-      #   user.regenerate_auth_token # => true
-      #
-      # SecureRandom::base58 is used to generate the 24-character unique token, so collisions are highly unlikely.
-      #
-      # Note that it's still possible to generate a race condition in the database in the same way that
-      # <tt>validates_uniqueness_of</tt> can. You're encouraged to add a unique index in the database to deal
-      # with this even more unlikely scenario.
+
       def has_secure_token(attribute = :token)
         # Load securerandom only when has_secure_token is used.
         require 'active_support/core_ext/securerandom'
@@ -32,7 +13,7 @@ module ActiveRecord
       end
 
       def generate_unique_secure_token
-        SecureRandom.base58(24)
+        SecureRandom.base58(24).downcase
       end
     end
   end
